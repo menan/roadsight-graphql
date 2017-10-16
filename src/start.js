b0ba49a52bb24cc1cf18c2f325fb61905dc0cc93
Menan Vadivel <menan.vadivel@gmail.com>
2017-10-16 00:10:25 -0400

    intial commit

--- src/start.js
+++ src/start.js
@@ -4,10 +4,13 @@ import bodyParser from 'body-parser'
 import {graphqlExpress, graphiqlExpress} from 'graphql-server-express'
 import {makeExecutableSchema} from 'graphql-tools'
 import cors from 'cors'
+import {
+	GraphQLScalarType
+} from 'graphql';
 
 const URL = 'http://localhost'
 const PORT = 3001
-const MONGO_URL = 'mongodb://localhost:27017/blog'
+const MONGO_URL = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
 const prepare = (o) => {
   o._id = o._id.toString()
@@ -18,33 +21,35 @@ export const start = async () => {
   try {
     const db = await MongoClient.connect(MONGO_URL)
 
-    const Posts = db.collection('posts')
-    const Comments = db.collection('comments')
+    const Reports = db.collection('reports')
 
     const typeDefs = [`
+      scalar Date
+  
       type Query {
-        post(_id: String): Post
-        posts: [Post]
-        comment(_id: String): Comment
+        report(_id: String): Report
+        reports: [Report]
       }
 
-      type Post {
+      type Report {
         _id: String
-        title: String
-        content: String
-        comments: [Comment]
+        status: String
+        date: Date
+        source: String
+        votes: Int
+        location: Location
       }
 
-      type Comment {
-        _id: String
-        postId: String
-        content: String
-        post: Post
+      type Location {
+        lat: Float
+        lng: Float
+        placeName: String
       }
 
       type Mutation {
-        createPost(title: String, content: String): Post
-        createComment(postId: String, content: String): Comment
+        createReport(status: String, source: String, lat: Float, lng: Float, placeName: String): Report
+        upVote(_id: String): Report
+        downVote(_id: String): Report
       }
 
       schema {
@@ -54,35 +59,54 @@ export const start = async () => {
     `];
 
     const resolvers = {
+      Date: new GraphQLScalarType({
+        name: 'Date',
+        description: 'Date custom scalar type',
+        parseValue(value) {
+          return new Date(value); // value from the client
+        },
+        serialize(value) {
+          return value // value sent to the client
+        },
+        parseLiteral(ast) {
+          if (ast.kind === Kind.INT) {
+            return parseInt(ast.value, 10) // ast value is always in string format
+          }
+          return null;
+        },
+      }),
       Query: {
-        post: async (root, {_id}) => {
-          return prepare(await Posts.findOne(ObjectId(_id)))
+        report: async (root, {_id}) => {
+          return prepare(await Reports.findOne(ObjectId(_id)))
         },
-        posts: async () => {
-          return (await Posts.find({}).toArray()).map(prepare)
+        reports: async () => {
+          return (await Reports.find({}).toArray()).map(prepare)
         },
-        comment: async (root, {_id}) => {
-          return prepare(await Comments.findOne(ObjectId(_id)))
-        },
-      },
-      Post: {
-        comments: async ({_id}) => {
-          return (await Comments.find({postId: _id}).toArray()).map(prepare)
-        }
-      },
-      Comment: {
-        post: async ({postId}) => {
-          return prepare(await Posts.findOne(ObjectId(postId)))
-        }
       },
       Mutation: {
-        createPost: async (root, args, context, info) => {
-          const res = await Posts.insert(args)
-          return prepare(await Posts.findOne({_id: res.insertedIds[1]}))
+        createReport: async (root, args, context, info) => {
+          const report = {
+            status: args.status,
+            date: new Date(),
+            source: args.source,
+            votes: 0,
+            location: {
+              lat: args.lat,
+              lng: args.lng,
+              placeName: args.placeName,
+            }
+          }
+
+          const res = await Reports.insert(report)
+          return prepare(await Reports.findOne({_id: res.insertedIds[1]}))
         },
-        createComment: async (root, args) => {
-          const res = await Comments.insert(args)
-          return prepare(await Comments.findOne({_id: res.insertedIds[1]}))
+        upVote: async (root, {_id}) => {
+          const report = await Reports.update({_id: ObjectId(_id)}, {$inc: {votes: 1}})
+          return prepare(await Reports.findOne(ObjectId(_id)))
+        },
+        downVote: async (root, {_id}) => {
+          const report = await Reports.update({_id: ObjectId(_id)}, {$inc: {votes: -1}})
+          return prepare(await Reports.findOne(ObjectId(_id)))
         },
       },
     }
